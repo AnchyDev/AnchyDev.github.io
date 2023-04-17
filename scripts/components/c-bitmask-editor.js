3d8fe26d4f75482ea193aabc086ae27e7a4b08b1
AnchyDev <lewienato@gmail.com>
2023-04-17 17:26:43 +1000

    Further work on bitmask editor.

--- scripts/components/c-bitmask-editor.js
+++ scripts/components/c-bitmask-editor.js
@@ -19,7 +19,86 @@ function hideError()
     }
 }
 
+function getBitmaskDataFromInput()
+{
+    let bitmaskDataElement = document.getElementById("bitmask-data");
+    if(!bitmaskDataElement)
+    {
+        return null;
+    }
+
+    let bitmaskData = bitmaskDataElement.value;
+    if(!bitmaskData)
+    {
+        return null;
+    }
+
+    return bitmaskData;
+}
+
+function getBitmaskOptions(bitmaskData)
+{
+    let decodedData = atob(bitmaskData).split(";");
+    return decodedData;
+}
+
+function getMaskFromIndex(index)
+{
+    return 1 << index;
+}
+
+function getMaskFromOptions(options)
+{
+    let mask = 0;
+    for(let option in options)
+    {
+        let optionElement = document.getElementById("bitmask-checkbox-" + option);
+        if(optionElement.checked)
+        {
+            mask += 1 << option;
+        }
+    }
+    return mask;
+}
+
 function bitmaskEditOnClick() 
 {
-    displayError("An unexpected error occurred!");
+    let bitmaskData = this.getBitmaskDataFromInput();
+    if(!bitmaskData)
+    {
+        displayError("Invalid bitmask data string.");
+        return;
+    }
+
+    let bitmaskOptions = this.getBitmaskOptions(bitmaskData);
+    if(bitmaskOptions.length < 1)
+    {
+        displayError("An unexpected error occurred.");
+        return;
+    }
+
+    let bitmaskOpsElement = document.getElementById("bitmask-options");
+    for(let option in bitmaskOptions)
+    {
+        let bitmaskOpElement = document.createElement("input");
+        bitmaskOpElement.id = `bitmask-checkbox-${option}`;
+        bitmaskOpElement.type = "checkbox";
+        bitmaskOpsElement.appendChild(bitmaskOpElement);
+
+        let bitmaskOpLabelElement = document.createElement("label");
+        bitmaskOpLabelElement.innerHTML = ` (${getMaskFromIndex(option)}): ${bitmaskOptions[option]}`;
+        bitmaskOpsElement.appendChild(bitmaskOpLabelElement);
+
+        bitmaskOpsElement.appendChild(document.createElement("br"));
+    }
+
+    let bitmaskSubmitElement = document.getElementById("bitmask-completer");
+    let bitmaskSubmitButtonElement = document.createElement("button");
+    bitmaskSubmitButtonElement.innerHTML = "Generate";
+    bitmaskSubmitButtonElement.className = "nice-button";
+    bitmaskSubmitButtonElement.onclick = function() {
+        let resultElement = document.getElementById("bitmask-result");
+        resultElement.innerHTML = `Result: ${getMaskFromOptions(bitmaskOptions)}`
+    };
+    bitmaskSubmitElement.appendChild(bitmaskSubmitButtonElement);
 }
